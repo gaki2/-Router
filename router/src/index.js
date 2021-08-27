a8a4d8458e7e18b589c81df3b2cbdc6f004b3ea7
ByeonggakYu <qudrkr0815@korea.ac.kr>
2021-08-27 15:11:08 +0900

    [practice]

--- router/src/index.js
+++ router/src/index.js
@@ -3,52 +3,37 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, NavLink, Switch, useParams } from 'react-router-dom';
 import './index.css'
 
+let contents = [
+  {id : 1, title : '강민지' , content: '민지 ㅎ'},
+  {id : 2, title: '소소소', content: '모모모'},
+  {id : 3, title: '키키키' , content: '카카카'},
+]
+
+function Topics() {
+  let lis = [];
+  for (let i = 0; i < contents.length; i++) {
+    lis.push(<li><NavLink to={`/topics/${contents[i].id}`}>{contents[i].title}</NavLink></li>)
+  }
+
+  let routers = [];
+  for (let i = 0; i < contents.length; i++) {
+    routers.push(<Route path={`/topics/${contents[i].id}`}>{contents[i].content}</Route>)
+  }
+  return (
+    <div>
+       <ul>
+        {lis}
+      </ul>
+      {routers}
+    </div>
+  )
+}
 
 function Contact() {
   return (
     <div>
       <h1>Contact</h1>
       Contact is contact ...
-    </div>
-  )
-}
-
-let contents = [
-  {id : 1, title : '유병각', content: '성공한다'},
-  {id : 2, title : '유병각', content: '성공한 남자'},
-  {id : 3, title : '유병각', content: '성공했다'},
-];
-
-function Topic() {
-  let content_id = useParams().content_id;
-  console.log(content_id);
-  let content = 'no content';
-  for (let i = 0; i < contents.length; i++) {
-    if (Number(content_id) === contents[i].id) {
-      content = contents[i].content;
-      break ;
-    }
-  }
-  return (
-    <div>
-      <h3>{content}</h3>
-    </div>
-  )
-}
-
-
-function Topics() {
-  let lis = [];
-  for (let i = 0; i < contents.length; i++) {
-    lis.push(<li><NavLink to={`/topics/${contents[i].id}`}>{contents[i].title}</NavLink></li>);
-  }
-  return (
-    <div>
-      <h1>Topics</h1>
-      <ul>
-        {lis}
-      </ul>
-      <Route path='/topics/:content_id'><Topic></Topic></Route>
     </div>
   )
 }
@@ -62,25 +47,24 @@ function Home() {
   )
 }
 
-function App() {
+function App () {
   return (
     <div>
-      <h1>React Router DOM Example</h1>
+      <h1>React Router DOM example ...</h1>
       <ul>
         <li><NavLink exact to='/'>Home</NavLink></li>
-        <li><NavLink to='/topics'>Topics</NavLink></li>
         <li><NavLink to='/contact'>Contact</NavLink></li>
+        <li><NavLink to='/topics'>Topics</NavLink></li>
       </ul>
       <Switch>
-        <Route exact path='/'><Home></Home></Route>
-        <Route path='/topics'><Topics></Topics></Route>
-        <Route path='/contact'><Contact></Contact></Route>
-        <Route path='/'>No page</Route>
+      <Route exact path='/'><Home></Home></Route>
+      <Route path='/contact'><Contact></Contact></Route>
+      <Route path='/topics'><Topics></Topics></Route>
+      <Route path='/'><h1>No page</h1></Route>
       </Switch>
     </div>
   )
 }
-
 
 
 
